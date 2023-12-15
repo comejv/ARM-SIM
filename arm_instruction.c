885de0da51639097644eb5e6aeccdc64931c5aec
Thomas Civade <thomas.civade@gmail.com>
2023-12-15 16:38:09 +0100

    starting decode inst code (inscode ==0 et 1 remain)

--- arm_instruction.c
+++ arm_instruction.c
@@ -16,9 +16,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
 �tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 B�timent IMAG
-	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'H�res
+     B�timent IMAG
+     700 avenue centrale, domaine universitaire
+     38401 Saint Martin d'H�res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -28,17 +28,121 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_constants.h"
 #include "util.h"
 
-static int arm_execute_instruction(arm_core p) {
-    uint32_t inst;
-    int result = arm_fetch(p, &inst);
-    
+static int arm_fetch_code_inst(arm_core p, uint32_t inst)
+{
+    uint8_t instcode = (inst & INSTRUCTION_MASK) >> INSTRUCTION_SHIFT;
+    uint8_t b24 = (inst & (B24_MASK) >> (B24_SHIFT));
+    uint8_t opcode = (inst & OPCODE_MASK) >> OPCODE_SHIFT;
+    uint8_t S = (inst & S_MASK) >> S_SHIFT;
+    uint8_t b4 = (inst & B4_MASK) >> B4_SHIFT;
+    uint8_t b20 = (inst & L_MASK) >> L_SHIFT;
+
+    if (instcode == 0 && (opcode >> 2) == 2 && S == 0)
+    {
+        // Miscellaneous
+    }
+    else if (instcode == 8)
+    {
+        if (b24 == 1)
+        {
+            // Software interrupt
+        }
+        else
+        {
+            // Coprocessor
+        }
+    }
+    else if (instcode == 5)
+    {
+        // Branch and branch with link
+    }
+    else if (instcode == 4)
+    {
+        // Load/Store multiple
+    }
+    else if (instcode == 3)
+    {
+        if (b4 == 0)
+        {
+            // Load/store register offset
+        }
+        else
+        {
+            // Media instructions / Undefined architecture
+        }
+    }
+    else if (instcode == 2)
+    {
+        // Load/store immediate offset
+    }
+    else if (instcode == 1)
+    {
+        
+    }
 }
 
-int arm_step(arm_core p) {
+static int arm_execute_instruction(arm_core p)
+{
+    uint32_t inst;
+    int result = arm_fetch(p, &inst);
+    if (result == 0)
+    {
+        uint8_t cond_inst = (inst & CONDITION_MASK) >> CONDITION_SHIFT;
+        uint32_t cpsr = arm_read_cpsr(p);
+        int FLAG_COND = 0;
+        uint8_t cpsr_Z = (cpsr >> Z) & 0x1;
+        uint8_t cpsr_N = (cpsr >> N) & 0x1;
+        uint8_t cpsr_C = (cpsr >> C) & 0x1;
+        uint8_t cpsr_V = (cpsr >> V) & 0x1;
+        if (cond_inst == EQ && Z == 1)
+            FLAG_COND = 1;
+        else if (cond_inst == NE && Z == 0)
+            FLAG_COND = 1;
+        else if (cond_inst == CS && C == 1)
+            FLAG_COND = 1;
+        else if (cond_inst == CC && C == 0)
+            FLAG_COND = 1;
+        else if (cond_inst == MI && N == 1)
+            FLAG_COND = 1;
+        else if (cond_inst == PL && N == 0)
+            FLAG_COND = 1;
+        else if (cond_inst == VS && V == 1)
+            FLAG_COND = 1;
+        else if (cond_inst == VC && V == 0)
+            FLAG_COND = 1;
+        else if (cond_inst == HI && C == 1 && Z == 0)
+            FLAG_COND = 1;
+        else if (cond_inst == LS && C == 1 && Z == 1)
+            FLAG_COND = 1;
+        else if (cond_inst == GE && N == V)
+            FLAG_COND = 1;
+        else if (cond_inst == LT && N != V)
+            FLAG_COND = 1;
+        else if (cond_inst == GT && Z == 0 && N == V)
+            FLAG_COND = 1;
+        else if (cond_inst == LE && (Z == 1 || N != V))
+            FLAG_COND = 1;
+        else if (cond_inst == AL)
+            FLAG_COND = 1;
+
+        if (FLAG_COND)
+        {
+            return arm_fetch_code_inst(p, inst);
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
+
+int arm_step(arm_core p)
+{
     int result;
 
     result = arm_execute_instruction(p);
-    if (result) {
+    if (result)
+    {
         return arm_exception(p, result);
     }
     return result;
