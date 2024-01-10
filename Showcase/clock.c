cffa38ecc3667895e6c6470da1e0bb01bde9959f
Thomas Civade <thomas.civade@gmail.com>
2024-01-11 00:14:14 +0100

    force a jump to be at 0x18

--- Showcase/clock.c
+++ Showcase/clock.c
@@ -2,13 +2,27 @@
 
 void message()
 {
-    my_putchar('I');{}
+    my_putchar('I');
     my_putchar('\n');
+}
+
+void irq_handler(void)
+{
+    message();
+    acquitter_irq();
+}
+
+void __attribute__((section(".irq_vector"), naked)) irq_vector(void)
+{
+    asm volatile(
+        "b irq_handler"
+    );
 }
 
 int main()
 {
-  while (1){}
+    while (1)
+    {
+    }
     return 0;
-
 }
